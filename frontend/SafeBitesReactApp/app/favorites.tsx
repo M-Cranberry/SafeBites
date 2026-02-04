855905aa260a586377932cb92456856915edfb76
M-Carberry <me356682@ucf.edu>
2026-02-04 04:54:12 -0500

    Small edits to fix a few accuracy issues on other pages and half of profile is set up and generally styled.

--- frontend/SafeBitesReactApp/app/favorites.tsx
+++ frontend/SafeBitesReactApp/app/favorites.tsx
@@ -159,7 +159,7 @@ const styles = StyleSheet.create({
   discoverTitle: {
     fontSize: 38,
     fontWeight: "600",
-    color: "#8AA197",
+    color: "#719F91",
     paddingBottom: 10,
     marginLeft: 10,
   },
